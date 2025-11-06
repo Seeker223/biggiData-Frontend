87c35c5c10bdd07fe3b34399bf69ba043c6febf7
BADUN OLUWATOBI <160877192+Seeker223@users.noreply.github.com>
2025-11-06 08:41:28 +0100

    updated

---
+++ constants/font.js
@@ -0,0 +1,5 @@
+export const fontMap = {
+  "Red Rose": require("../assets/fonts/RedRose-Bold.ttf"),
+  "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+};
---
+++ constants/images.js
@@ -0,0 +1,12 @@
+import profile from "../assets/images/profile.png";
+import thumbnail from "../assets/images/thumbnail.png";
+import cards from "../assets/images/cards.png";
+import path from "../assets/images/path.png";
+import logo from "../assets/images/logo.png";
+import logoSmall from "../assets/images/logo-small.png";
+import empty from "../assets/images/empty.png";
+import biggiData2 from "../assets/images/biggiData2.png";
+import biggiData1 from "../assets/images/biggiData1.png";
+import biggiSplash from "../assets/images/BiggiData_SplashScreen.png"
+
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty, biggiData2, biggiData1,biggiSplash };
